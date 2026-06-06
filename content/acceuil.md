9613dcab24fda63a94fda7bb9cd846674d7446e0
Louis Cagnion <louis.cagnion.pro@gmail.com>
2026-06-07 01:23:45 +0200

    home page displayed

---
+++ content/acceuil.md
@@ -0,0 +1,5 @@
+---
+title: Acceuil
+---
+
+Ce site web est dedier a la restitution de mes connaissances en programmation.
--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,4 @@
+import { parseAppendText, parseMdContent } from "./parser.js";
 import { createTag } from "./tags.js";
 
 /**
@@ -57,6 +58,18 @@ function generateNavBar(categories = []) {
     document.body.append(navBar);
 }
 
+async function generateHomePage(homeFileName) {
+    const homeFile = await fetch(`./content/${homeFileName}.md`, {
+        headers: {
+            Accept: "text/plain"
+        }
+    });
+    const homeInfos = await homeFile.text();
+    const [yaml, text] = parseMdContent(homeInfos);
+    const homeDiv = createTag("div", {class: `${homeFileName}Div`});
+    parseAppendText(homeDiv, homeFileName, yaml, text);
+    document.body.append(homeDiv);
+}
 
 async function fetchStructJson(structPath = "./structure/struct.json") {
     const struct = await fetch(structPath, {
@@ -66,6 +79,7 @@ async function fetchStructJson(structPath = "./structure/struct.json") {
     });
     let dataJson = await struct.json();
     generateNavBar(dataJson.categories);
+    generateHomePage(dataJson.categories[0].id);
 }
 
 fetchStructJson();
