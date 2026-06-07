47a7260d5a2dfe0f8766b25ab860f88f58a43419
Louis Cagnion <louis.cagnion.pro@gmail.com>
2026-06-07 21:21:02 +0200

    phone UI : menu bar is working

--- content/acceuil.md
+++ content/acceuil.md
@@ -1,5 +1,13 @@
 ---
-title: Acceuil
+title: Accueil
 ---
 
-Ce site web est dedier a la restitution de mes connaissances en programmation.
+Bienvenue sur **Devpedia**, ma base de connaissances personnelle en développement.
+
+Ce site regroupe mes notes et apprentissages sur différents sujets :
+* langages de programmation,
+* langages de balisage,
+* scripting Bash,
+* et plus encore.
+
+Vous retrouverez les differentes catégorie dans la barre de navigation ci-dessus.
--- js/tags.js
+++ js/tags.js
@@ -2,15 +2,16 @@
  * Create any HTML tag
  * @param {string} tagName 
  * @param {object<key, values>} attributes 
- * @param {string} body 
+ * @param {object<key, values>} DOMProperties 
  * @returns {HTMLElement}
  */
-export function createTag(tagName= "", attributes = {}, body = "") {
+export function createTag(tagName= "", attributes = {}, DOMProperties = {}) {
     if (!tagName)
             return null;
     let tag = document.createElement(tagName);
-    if (body && body !== "undefined")
-        tag.textContent = body
+    Object.entries(DOMProperties).forEach(([key, value]) => {
+        tag[key] = value;
+    })
     Object.entries(attributes).forEach(([key, value]) => {
         tag.setAttribute(key, value);
     })
